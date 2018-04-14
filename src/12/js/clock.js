9c7b9eefe5298b19598ba03755870f1f1f3fa125
bencode <bencode@163.com>
2018-04-15 00:41:16 +0800

    完成12课clock的review

--- src/12/js/clock.js
+++ src/12/js/clock.js
@@ -4,7 +4,12 @@ const hourHand=oClock.querySelector('.hour-hand');
 const minuteHand=oClock.querySelector('.minute-hand');
 const secondHand=oClock.querySelector('.second-hand');
 const dataTime=oClock.querySelector('.date-time');
+
+// 这个自己发挥得很棒！
 //四周原点
+// 用了es6，那咱们整个文件都用const或let
+// const和let的使用原则是： 能用const的地方就用const, 只有需要重新赋值的地方才用let。
+// 所以下面的for语句里面的i，需要使用let， 但是 rad, l 和 t都使用const来定义。
 for(var i=0;i<12;i++){
     let rad=i*30*Math.PI/180;
     let l=140*Math.cos(rad)+150;
@@ -14,12 +19,15 @@ for(var i=0;i<12;i++){
 //获取时间
 setInterval(changeTime,1000);
 function changeTime(){
+    // 变量名要用驼峰形式命名。
     let NewD=new Date();
 
     let NewH=''+NewD.getHours();
     let NewM=''+NewD.getMinutes();
     let NewS=''+NewD.getSeconds();
 
+    // 你可以尝试自己写一个padStart函数吗？因为有些浏览器下不支持padStart
+    // 顺便查一些哪些情况下不支持, 注意手机中的浏览器
     dataTime.innerHTML=NewH.padStart(2,'0')+':'+NewM.padStart(2,'0')+':'+NewS.padStart(2,'0');
 
     let s=parseInt(NewS)*30/5-180;

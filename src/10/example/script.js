c063897924eb81b9ad679e1b5d9a7ee9f127e8d8
bencode <bencode@163.com>
2018-04-23 14:53:04 +0800

    finish review

--- src/10/example/script.js
+++ src/10/example/script.js
@@ -33,23 +33,37 @@
 
 // }
 
-fuction addText() {
+function addText() {
     var color = document.querySelector('.color-mixer');
     var indicator = color.querySelector('.indicator');
 
-    var red = document.querySelector('.adjuster .red');
+    // 这个取到的不是输入框哦，所以你得检查下取到的元素
+    // 可以使用console.log来确定
+    // var red = document.querySelector('.adjuster .red');
+    var red = document.querySelector('.adjuster .red input');
     var red = red.value;
 
-    var gree = document('.adjuster .green');
-    var gree = gree.value;
+    // var gree = document('.adjuster .green');
+    var green = document.querySelector('.adjuster .green input');
+    var green = green.value;
 
-    var blue = document.querySelector('.adjuster .blue');
+    var blue = document.querySelector('.adjuster .blue input');
     var blue = blue.value;
 
     var lastHtml = indicator.innerHTML;
-    indicator.innerHTML = lastHtml + '<span style="red:' + red + ';green:' + green + ';blue:' + blue + '"></span>';
+    // 这次不像富文本那样修改内容,而是设置样式，你参考上面你注释掉的代码来设置样式哈。
+    // indicator.innerHTML = lastHtml + '<span style="red:' + red + ';green:' + green + ';blue:' + blue + '"></span>';
 
+    var css = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    // 可以尝试使用console.log()来输出css，看是否正确。
+    console.log(css);
+    indicator.style.background = css;
 }
 
+// 现在你已绑定了一个颜色条的事件。 所以第一个颜色框已能正常工作了。
+// 还需要优化
+// 1. 这里的颜色选择框事件不叫click，而是叫 change， click是鼠标点击的意思。
+// 2. 还可以尝试使用事件 input, 你可以尝试一下。
+// 3. 把其他两个颜色框也加上事件。
 var addInput = document.querySelector('.body input');
 addInput.addEventListener('click', addText);

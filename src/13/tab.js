eec4b6afafa9aa8d95886b15247fa8230465a046
Snow-pear <958427071@qq.com>
2018-05-03 14:07:27 +0800

    tab.js

--- src/13/tab.js
+++ src/13/tab.js
@@ -1,6 +1,28 @@
-var tab = document.querySelector('.tab-menu');
-var li = tab.querySelector('li');
-var lis = tab.querySelectorAll('li');
+function initTabMenu() {
+    var tab = document.querySelector('.tab-menu');
+    var li = tab.querySelector('li');
+    var lis = tab.querySelectorAll('li');
+    var contains = tab.querySelector('.contains');
 
-console.log('li');
-console.log('lis');
+    // console.log(li);
+    // console.log(lis);
+
+    for (var i = 0; i < lis.length; i++) {
+        console.log(contains.innerHTML);
+        contains.innerHTML = lis[i].innerHTML;
+        addEvent(i);
+
+    }
+
+    function addEvent(index) {
+        lis[index].addEventListener('click', function () {
+            for (var i = 0; i < lis.length; i++) {
+                lis[i].classList.remove('active');
+            }
+            lis[index].classList.add('active');
+        });
+    }
+
+}
+
+initTabMenu();

be976c2fc1c1ed60f6682ae1fc1226e1fd117bcf
Snow-pear <958427071@qq.com>
2018-05-09 15:13:57 +0800

    update

--- src/14/test.js
+++ src/14/test.js
@@ -2,6 +2,7 @@ var Text = document.querySelector('.text-input');
 var Btn = document.querySelector('.add-btn');
 
 Btn.addEventListener('click', function () {
+    console.log('hello', value);
     var value = Text.value;
     if (!value) {
         alert('请输入内容！');

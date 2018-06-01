2fc1b31d9a35b9daed02e3d43b3d229ad5ab74aa
Snow-pear <958427071@qq.com>
2018-06-01 20:11:05 +0800

    完成top-bar

---
+++ src/douban/index.js
@@ -0,0 +1,17 @@
+initMenu();
+
+function initMenu() {
+    var menu = document.querySelector('.top-bar .menu');
+    var id = menu.querySelector('.id');
+    var items = menu.querySelector('.items');
+
+    id.addEventListener('click', function () {
+        console.log('hello');
+
+        if (menu.classList.contains('active')) {
+            menu.classList.remove('active');
+        } else {
+            menu.classList.add('active');
+        }
+    });
+}
